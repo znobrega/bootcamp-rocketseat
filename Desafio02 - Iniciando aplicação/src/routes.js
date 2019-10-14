70d2bc375c0b7a5a9092dab5d5b37ce61e4a7b0c
Carlos Nobrega <nobregacarlosjr@gmail.com>
2019-10-14 03:14:13 -0300

    password hash and authentication with jwt

--- Desafio02 - Iniciando aplicação/src/routes.js
+++ Desafio02 - Iniciando aplicação/src/routes.js
@@ -1,7 +1,19 @@
 import express from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import User from './app/models/User';
+
+import authMiddleware from './app/middlewares/authMiddleware';
+
 const routes = new express.Router();
 
-routes.get('/', (req, res) => res.send('Hey'));
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/update', UserController.update);
 
 export default routes;
