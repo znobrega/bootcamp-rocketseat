c060d5a833580a53849930e4a6c27f6b37d583dd
Carlos Nobrega <nobregacarlosjr@gmail.com>
2019-10-26 22:20:21 -0300

    multer config fixed

--- Desafio02 - Iniciando aplicação/src/config/multer.js
+++ Desafio02 - Iniciando aplicação/src/config/multer.js
@@ -5,7 +5,7 @@ import crypto from 'crypto';
 export default {
   storage: multer.diskStorage({
     destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-    filename: (req, file, cb) {
+    filename: (req, file, cb) => {
       crypto.randomBytes(16, (err, res) => {
         if(err) return cb(err)
         cb(null, res.toString('hex') + extname(filename))
--- Desafio03 - Continuando aplicação/src/config/multer.js
+++ Desafio03 - Continuando aplicação/src/config/multer.js
@@ -5,11 +5,11 @@ import crypto from 'crypto';
 export default {
   storage: multer.diskStorage({
     destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-    filename: (req, file, cb) {
+    filename: (req, file, cb) => {
       crypto.randomBytes(16, (err, res) => {
-        if(err) return cb(err)
-        cb(null, res.toString('hex') + extname(filename))
-      })
-    }
+        if (err) return cb(err);
+        cb(null, res.toString('hex') + extname(filename));
+      });
+    },
   }),
 };
